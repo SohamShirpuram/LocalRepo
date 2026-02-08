01ada180f22daec432cb3c6e85156073d0e5ef74
Soham Shirpuram <sohamshirpuram@gmail.com>
2026-02-08 19:05:53 +0530

    completed else

---
+++ Pyion/Pyion.py
@@ -0,0 +1,70 @@
+#name = input("What is your name? :")
+#print("Hello" + name)
+#name ="Soham"
+#first_name = name[0:4]
+#print(first_name)
+
+#website1 = "http://google.com"
+#website2 = "http://wikipedia.com"
+
+#slice = slice(7,-4)
+
+#print(website1[slice])
+#print(website2[slice])
+
+#name2 = print(input("What is your name? :"))
+#(print ("Hello " , name2))
+
+#name2 = input("What is your name? : ")
+#print("Hello :", name2)
+
+#name3 = input("What is your name? :")
+#print("Hello " + name3)
+
+#print("To Calculate, Enter the numbers")
+##X = (int(input("X = ")))
+##Y = (int(input("Y = ")))
+
+##print("Which operation")
+##Z = input()
+
+##if Z = +;##
+##     print(X + Y)
+##elif Z == - :
+##    print(X - Y)
+##elif Z == *:
+##     print(X * Y)
+##elif Z == /:
+##    print(X/Y)  
+
+##else:
+ ##    print("X" = X "," "Y" = Y )
+
+
+#print("To calculate, enter the numbers")
+
+#x = int(input("Enter x: "))
+#y = int(input("Enter y: "))
+#
+#print("Which operation")
+#print("1 +")
+#print("2 -")
+#print("3 *")
+#print("4 /")
+#
+#z = int(input("Choose option: "))
+#
+#if z == 1:
+#    print(x + y)
+#elif z == 2:
+#    print(x - y)
+#elif z == 3:
+#    print(x * y)
+#elif z == 4:
+#    if y != 0:
+#        print(x / y)
+#    else:
+#        print("Cannot divide by zero")
+#else:
+#    print("Invalid choice")
+#
